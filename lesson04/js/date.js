53b7b88b5d2052c95958f3207eca2188158ba8dd
chrisj63-git <joh20121@byui.edu>
2021-02-02 21:48:18 -0700

    up4

---
+++ lesson04/js/date.js
@@ -0,0 +1,42 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+fulldate = formatDate(d);
+
+document.getElementById("currdate").textContent = fulldate;
+
+let c = new Date().getFullYear()
+document.getElementById("copydate").textContent = c;
+
+function formatDate (d) {
+    
+    let dayName = daynames[d.getDay()];
+    let monthName = months[d.getMonth()];
+    let year = d.getFullYear();
+    let fulldate =  dayName + ", " + monthName + " " + d.getDate() + ", " + year;
+
+    return fulldate;
+}
