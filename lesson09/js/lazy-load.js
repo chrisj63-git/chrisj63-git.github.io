c6c50d5f7ca7cdb0b959ad02e1936f6077cb641a
chrisj63-git <joh20121@byui.edu>
2021-03-17 19:44:42 -0600

    less9

---
+++ lesson09/js/lazy-load.js
@@ -0,0 +1,31 @@
+let imagesToAdd = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+    threshhold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = function(){image.removeAttribute("data-src");};
+};
+
+if('IntersectionObserver' in window){
+const imgObserver = new IntersectionObserver((items, imgObserver) => {
+    items.forEach((item) => {
+        if (!item.isIntersecting) {
+            return;
+        } else {
+            loadImages(item.target);
+            imgObserver.unobserve(item.target);
+        }   
+    });
+  }, imgOptions);
+    imagesToAdd.forEach((image) => {
+    imgObserver.observe(image); 
+});
+} else {
+    imagesToAdd.forEach((image) =>{
+      loadImages(image);  
+    }); 
+}
---
+++ lesson09/js/prophets.js
@@ -0,0 +1,36 @@
+const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+    const cards = document.querySelector('.cards');
+
+    prophets.forEach(prophet => {
+      
+     
+ 
+let card = document.createElement('section');
+let h2 = document.createElement('h2');
+let h3 = document.createElement('h3');
+let p = document.createElement('p');
+let image = document.createElement('img');
+
+h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+h3.innerHTML = `Date of Birth: ${prophet.birthdate}`;
+p.innerHTML = `Place of Birth: ${prophet.birthplace}`;
+image.setAttribute('src', prophet.imageurl);
+image.setAttribute('alt', h2.textContent + ` - ${prophet.order}`);
+
+card.append(h2);
+cards.append(card);
+card.append(h3);
+card.append(p);
+card.append(image);
+
+    
+    });
+  });
