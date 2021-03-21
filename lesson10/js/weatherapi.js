efaca627e7ebbcfaf9ebd055100307da9e111933
chrisj63-git <joh20121@byui.edu>
2021-03-20 22:46:54 -0600

    }

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -2,14 +2,12 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-   // console.log(jsObject);
 	const temperature = document.querySelector('#current-temp');
 	temperature.textContent = jsObject.main.temp;
 //document.getElementById('current-temp').textContent = jsObject.main.temp;
-  });
-
 const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
 const desc = jsObject.weather[0].description;  
 document.getElementById('icon').textContent = imagesrc; 
 document.getElementById('icon').setAttribute('src', imagesrc);
 document.getElementById('icon').setAttribute('alt', desc);
+});
