170a95712eb6b44acb4ecb3a7ffd31a0c66c83e9
chrisj63-git <joh20121@byui.edu>
2021-03-20 23:24:19 -0600

    for

---
+++ lesson10/js/forcast.js
@@ -0,0 +1,19 @@
+let cityID = 5604473;
+let appID = 'c66865392f629eb0e6b62d563af8fd88';
+const apiURL = `https://api.openweathermap.org/data/2.5/forcast?id=${cityID}&appid=${appID}&units=imperial`;
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+console.log(apiURL);
+	
+ /*   
+    const temperature = document.querySelector('#current-temp');
+	temperature.textContent = jsObject.main.temp;
+//document.getElementById('current-temp').textContent = jsObject.main.temp;
+const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+const desc = jsObject.weather[0].description;  
+document.getElementById('imagesrc').textContent = imagesrc; 
+document.getElementById('icon').setAttribute('src', imagesrc);
+document.getElementById('icon').setAttribute('alt', desc);
+*/
+});
