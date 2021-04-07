79f35eb5119f6af4f0c995e942207ac4d0d0864d
chrisj63-git <joh20121@byui.edu>
2021-04-06 22:17:49 -0600

    jsonup

--- coc/js/members.js
+++ coc/js/members.js
@@ -10,20 +10,28 @@ fetch(requestURL)
     const cards = document.querySelector('.cards');
 
     members.forEach(member => {
-      
+    
 let card = document.createElement('section');
 let h2 = document.createElement('h2');
-let h3 = document.createElement('h3');
+let h3 = document.createElement('h3');``
 let h4 = document.createElement('h4');
 let h5 = document.createElement('h5');
-let p = document.createElement('p');
+let a = document.createElement('a');
+let href = document.createElement('href');
 let image = document.createElement('img');
+let currhtml = "<a href=" +`${member.website}` + ">" +`${member.website}` + "</a>";
+let currmail = "<a href='mailto:" +`${member.email}` + "'>" +`${member.email}` + "</a>";
+
+
 
 h2.innerHTML = `${member.name}`;
 h3.innerHTML = `${member.contact}`;
 h4.innerHTML = `${member.phone}`;
-h5.innerHTML = `${member.email}`;
-p.innerHTML = `${member.website}`;
+/*h5.innerHTML = `${member.email}`;
+href.innerHTML = `${member.website}`; */
+a.innerHTML = currhtml;
+h5.innerHTML = currmail;
+
 image.setAttribute('src', member.logo);
 image.setAttribute('alt', h2.textContent + ` - ${member.order}`);
 
@@ -33,8 +41,8 @@ cards.append(card);
 card.append(h3);
 card.append(h4);
 card.append(h5);
-card.append(p);
 card.append(image);
+card.append(a);
 
     
     });
