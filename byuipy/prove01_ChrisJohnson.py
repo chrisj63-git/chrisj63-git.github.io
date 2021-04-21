c14f4a2ca54df95d090541039e8167f45772897e
chrisj63-git <joh20121@byui.edu>
2021-04-20 20:00:09 -0600

    nex

--- byuipy/prove01_ChrisJohnson.py
+++ byuipy/prove01_ChrisJohnson.py
@@ -6,11 +6,11 @@ Purpose:  Python programming exercises for Week 1 lesson
 
 """
 name = input('What is your name? ')
-fcolor = input('What is your favorite color?')
-ice = input('What is your favorite flavor of ice cream?')
+fcolor = input('What is your favorite color? ')
+ice = input('What is your favorite flavor of ice cream? ')
 
 print()
-print('Hi ' + name + 'Nice to meet you. Your favorite color is ' + fcolor + 'and')
+print('Hi ' + name + ', Nice to meet you. Your favorite color is ' + fcolor + ' and')
 print('your favorite ice cream flavor is: ' + ice + '.')
 print('My name is Chris. My favorite color is green and my favorite ice cream flavor is "Moose Tracks".')
 print()
