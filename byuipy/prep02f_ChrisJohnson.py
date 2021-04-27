ae5f60a608682d688b01f21b589cd1d8d855b7dd
chrisj63-git <joh20121@byui.edu>
2021-04-26 21:58:52 -0600

    w28

--- byuipy/prep02f_ChrisJohnson.py
+++ byuipy/prep02f_ChrisJohnson.py
@@ -8,8 +8,7 @@ Purpose:  Python programming exercises for Week 2 prep
 fname = input('What is your first name? ')
 lname = input('What is your last name? ')
 
-print()
-print(f"Your name is {lname}, {fname} {lname}")
+print(f"Your name is {lname}, {fname} {lname}.")
 
 
 """
