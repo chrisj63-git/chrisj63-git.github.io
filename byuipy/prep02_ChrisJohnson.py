23e5b4d4b31a70db26d77569053f6e670065993a
chrisj63-git <joh20121@byui.edu>
2021-04-26 21:46:05 -0600

    w23

--- byuipy/prep02_ChrisJohnson.py
+++ byuipy/prep02_ChrisJohnson.py
@@ -9,7 +9,7 @@ fname = input('What is your first name? ')
 lname = input('What is your last name? ')
 
 print()
-'Your name is ' + lname + ', ' + fname + ' ' + lname + '.')
+print('Your name is ' + lname + ', ' + fname + ' ' + lname + '.')
 
 
 """
