69f787c115147b6116d32103c7c6d8b70d9fc773
chrisj63-git <joh20121@byui.edu>
2021-04-26 21:45:21 -0600

    w22

--- byuipy/prep02_ChrisJohnson.py
+++ byuipy/prep02_ChrisJohnson.py
@@ -7,8 +7,6 @@ Purpose:  Python programming exercises for Week 2 prep
 """
 fname = input('What is your first name? ')
 lname = input('What is your last name? ')
-fcolor = input('What is your favorite color? ')
-ice = input('What is your favorite flavor of ice cream? ')
 
 print()
 'Your name is ' + lname + ', ' + fname + ' ' + lname + '.')
