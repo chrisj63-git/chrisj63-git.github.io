f280a8b5f5c64d3887aaaf6a64fb86133e5ec8c8
chrisj63-git <joh20121@byui.edu>
2021-04-26 22:21:03 -0600

    w2p1

---
+++ byuipy/prove02_ChrisJohnson.py
@@ -0,0 +1,27 @@
+"""
+File: prep_02_ChrisJohnson.py
+Author: Chris Johnson
+
+"""
+dash-line = "----------------------------------------"
+print("Please enter the following information:\n")
+fname = input('First Name: ')
+lname = input('Last Name: ')
+email = input('Email address: ')
+phone = input('Phone number: ')
+jtitle = input('Job title: ')
+id = input('ID Number: ')
+
+print(f"\n{dash-line}\n")
+print(f"{jtitle}\n")
+print(f"{id}\n\n")
+print(f"{email}\n")
+print(f"{phone}\n")
+print(f"\n{dash-line}\n")
+
+"""
+Making it my own:
+
+
+
+"""
