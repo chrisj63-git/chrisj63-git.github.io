004da91fff9cad314f367e913cb960ed6114c01a
chrisj63-git <joh20121@byui.edu>
2021-04-26 22:00:11 -0600

    w29

--- byuipy/prep02f_ChrisJohnson.py
+++ byuipy/prep02f_ChrisJohnson.py
@@ -8,8 +8,7 @@ Purpose:  Python programming exercises for Week 2 prep
 fname = input('What is your first name? ')
 lname = input('What is your last name? ')
 
-print(f"Your name is {lname}, {fname} {lname}.")
-
+print(f"\nYour name is {lname}, {fname} {lname}.")
 
 """
 Making it my own:
