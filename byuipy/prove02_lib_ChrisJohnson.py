3cee0cf20d7db3dbaa8e7968de3c9f4ea30cf20c
chrisj63-git <joh20121@byui.edu>
2021-05-01 16:29:27 -0600

    lib4

--- byuipy/prove02_lib_ChrisJohnson.py
+++ byuipy/prove02_lib_ChrisJohnson.py
@@ -4,7 +4,7 @@ Author: Chris Johnson
 
 """
 
-print('Please enter the following:\n')
+print('\nPlease enter the following:\n')
 adj1 = input('adjective: ')
 animal = input('animal: ')
 verb1 = input('verb: ')
@@ -14,10 +14,10 @@ verb3 = input('verb: ')
 
 
 print('\nYour story is: \n')
-print('The other day, I was really in trouble. It all started when I saw a very \n')
-print(f'{adj1} {animal} {verb1} down the hallway. "{excl.capitalize()}!" I yelled. But all \n')
-print(f'I could think to do was to {verb2} over and over. Miraculously, \n')
-print(f'that caused it to stop, but not before it tried to {verb3} \n')
+print('The other day, I was really in trouble. It all started when I saw a very ')
+print(f'{adj1} {animal} {verb1} down the hallway. "{excl.capitalize()}!" I yelled. But all ')
+print(f'I could think to do was to {verb2} over and over. Miraculously, ')
+print(f'that caused it to stop, but not before it tried to {verb3} ')
 print(f'right in front of my family.\n\n')
 
 """
