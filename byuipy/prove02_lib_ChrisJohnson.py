7549d260ec84fcd56a0f9f491ef805aed8632873
chrisj63-git <joh20121@byui.edu>
2021-05-01 16:24:59 -0600

    lib2

--- byuipy/prove02_lib_ChrisJohnson.py
+++ byuipy/prove02_lib_ChrisJohnson.py
@@ -13,12 +13,12 @@ verb2 = input('verb: ')
 verb3 = input('verb: ')
 
 
-print('/nYour story is:/n')
-print('The other day, I was really in trouble. It all started when I saw a very /n')
-print(f'{adj1} {animal} {verb1} down the hallway. "{excl}!" I yelled. But all /n')
-print(f'I could think to do was to {verb2} over and over. Miraculously, /n')
-print(f'that caused it to stop, but not before it tried to {verb3} \n')
-print(f'right in front of my family./n/n')
+print(/n'Your story is:'/n)
+print('The other day, I was really in trouble. It all started when I saw a very '/n)
+print(f'{adj1} {animal} {verb1} down the hallway. "{excl.capitalize()}!" I yelled. But all '/n)
+print(f'I could think to do was to {verb2} over and over. Miraculously, '/n)
+print(f'that caused it to stop, but not before it tried to {verb3} '/n)
+print(f'right in front of my family.'/n/n)
 
 """
 Making it my own:
