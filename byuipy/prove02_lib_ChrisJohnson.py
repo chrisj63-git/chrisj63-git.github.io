4cb53d06a59bf7b9508e0bf5dba66100dfb879ef
chrisj63-git <joh20121@byui.edu>
2021-05-01 16:20:38 -0600

    lib1

---
+++ byuipy/prove02_lib_ChrisJohnson.py
@@ -0,0 +1,28 @@
+"""
+File: prove02_lib_ChrisJohnson.py
+Author: Chris Johnson
+
+"""
+
+print('Please enter the following:\n')
+adj1 = input('adjective: ')
+animal = input('animal: ')
+verb1 = input('verb: ')
+excl = input('exclamation: ')
+verb2 = input('verb: ')
+verb3 = input('verb: ')
+
+
+print('/nYour story is:/n')
+print('The other day, I was really in trouble. It all started when I saw a very /n')
+print(f'{adj1} {animal} {verb1} down the hallway. "{excl}!" I yelled. But all /n')
+print(f'I could think to do was to {verb2} over and over. Miraculously, /n')
+print(f'that caused it to stop, but not before it tried to {verb3} \n')
+print(f'right in front of my family./n/n')
+
+"""
+Making it my own:
+
+
+
+"""
