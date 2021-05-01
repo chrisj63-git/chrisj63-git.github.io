e1f2e41ef6729fedf983e38f2e72794bf5640060
chrisj63-git <joh20121@byui.edu>
2021-05-01 16:40:21 -0600

    libmy1

---
+++ byuipy/prove02_lib_my_ChrisJohnson.py
@@ -0,0 +1,36 @@
+"""
+File: prove02_lib_ChrisJohnson.py
+Author: Chris Johnson
+
+"""
+
+print('\nPlease enter the following:\n')
+adj1 = input('adjective: ')
+animal = input('animal: ')
+verb1 = input('verb: ')
+excl = input('exclamation: ')
+verb2 = input('verb: ')
+verb3 = input('verb: ')
+animal2 = input('animal: ')
+verb4 = input('verb: ')
+dessert = input('favorite dessert: ')
+
+
+print('\nYour story is: \n')
+print('The other day, I was really in trouble. It all started when I saw a very ')
+print(f'{adj1} {animal} {verb1} down the hallway. "{excl.capitalize()}!" I yelled. But all ')
+print(f'I could think to do was to {verb2} over and over. Miraculously, ')
+print(f'that caused it to stop, but not before it tried to {verb3} ')
+print(f'right in front of my family.\n')
+print(f'It was a good thing it was a holiday. ')
+print(f'My {animal2} was with me and chased the {animal} away. ')
+print(f'We all had a good laugh. Then we went home and had some {dessert} to celebrate \n\n')
+
+
+
+"""
+Making it my own:
+
+
+
+"""
